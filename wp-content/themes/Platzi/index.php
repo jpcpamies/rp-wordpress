e3f55cd3b6ff521b8f43985cee4c4733993f1e81
Jordi Pamies <jordipamies2@gmail.com>
2015-07-28 14:43:34 +0100

    Using the loop + query_post

--- wp-content/themes/Platzi/index.php
+++ wp-content/themes/Platzi/index.php
@@ -2,23 +2,59 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Document</title>
+	<title><?php bloginfo('description'); ?></title>
  	<link rel="stylesheet" href="<?php bloginfo(stylesheet_url) ?>">
 </head>
 <body>
-	<h1><?php bloginfo('name') ?></h1>
-	<h2><?php bloginfo('description') ?></h2>
-	<ul>
-		<li><?php bloginfo('name'); ?></li>
-		<li><?php bloginfo('description'); ?></li>
-		<li><?php bloginfo('wpurl'); ?></li>
-		<li><?php bloginfo('admin_email'); ?></li>
-		<li><?php bloginfo('charset'); ?></li>
-		<li><?php bloginfo('version'); ?></li>
-		<li><?php bloginfo('html_type'); ?></li>
-		<li><?php bloginfo('language'); ?></li>
-		<li><?php bloginfo('stylesheet_directory'); ?></li>
-		<li><?php bloginfo('stylesheet_url'); ?></li>
-	</ul>
+	<header class="header">
+		<h1><?php bloginfo('name') ?></h1>
+		<h2><?php bloginfo('description') ?></h2>
+	</header>
+	<section class="content">
+
+		<!-- 
+		* Esto se pone por seguridad para la estabilidad del loop
+	 	 -->
+		<?php rewind_posts(); ?>
+
+
+		<!-- 
+		* El query_post permite filrar por diferentes filtros
+		* Se coloca justo antes del loop
+		* Algunas opciones que puedo filtrar
+		** El orden de los post > order=Des Por defecto
+		** El orden de los post > order=Asc
+		** Categorias > order=Asc
+		*** Se puede filtrar por category name o por category id > recomendado
+		*** hacer por id > cat=4
+		* puedo añadir filtros concatenando con un & > order=Des&cat=4
+	 	 -->
+		<?php query_posts( 'order=Des' ); ?>
+		
+
+
+		<!-- 
+		* Esto es el loop
+		 -->
+		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+			<article class='post'>
+				<header>
+					<h3><?php the_title( ); ?></h3>
+					<div class="datos">
+						<strong><?php the_author( ); ?></strong>
+						<small><?php the_date(	 ); ?></small>
+					</div>
+				</header>
+				<?php the_excerpt(); ?>
+				<?php the_category( ); ?>
+			</article>
+		<!-- post -->
+		<?php endwhile; ?>
+		<!-- post navigation -->
+		<?php else: ?>
+		<!-- no posts found -->
+		<?php endif; ?>
+
+	</section>
 </body>
 </html>
