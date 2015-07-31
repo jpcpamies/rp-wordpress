85f3121f16a2ba08661b1c3397f449bd54d5a00e
Jordi Pamies <jordipamies2@gmail.com>
2015-07-31 14:22:03 +0100

    Creating a single page por post

--- wp-content/themes/Platzi/single.php
+++ wp-content/themes/Platzi/single.php
@@ -1,5 +1,35 @@
 <?php get_header(); ?>
 <body>
-	<h1>Esto es una entrada</h1>
+	<header class="header">
+		<h1><?php bloginfo('name') ?></h1>
+		<h2><?php bloginfo('description') ?></h2>
+	</header>
+	<div class="content">
+		<?php rewind_posts(); ?>
+		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+			<article class='post'>
+				<header>
+					<h3><a href="<?php the_permalink(); ?>"><?php the_title( ); ?></a></h3>
+					<div class="datos">
+						<strong><?php the_author( ); ?></strong>
+						<small><?php the_date(	 ); ?></small>
+					</div>
+				</header>
+				<figure class='img'>
+					<?php the_post_thumbnail( 'full' ); ?>
+				</figure>
+				<footer>
+					<?php the_excerpt(); ?>
+					<?php the_category( ); ?>
+				</footer>
+			</article>
+		<!-- post -->
+		<?php endwhile; ?>
+		<!-- post navigation -->
+		<?php else: ?>
+		<!-- no posts found -->
+		<?php endif; ?>
+
+	</div>
 </body>
 </html>
